c3cf45581d0c825ab90e783a8b0f1208cfb02f4f
Mmaserufe Sekalo <maserufec@gmail.com>
2023-01-11 17:25:34 +0200

    added ids

--- week7/src/app.js
+++ week7/src/app.js
@@ -1,15 +1,16 @@
-let cityElement = document.querySelector("#city");
-cityElement.innerHTML = response.data.name;
+function tempDisplay(response) {
+  let city = document.querySelector("#city");
+  let temeperaturElement = document.querySelector("#temperature");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  city.innerHTML = response.data.city;
+  temeperaturElement.innerHTML = Math.round(response.data.temperature.current);
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = response.data.temperature.humidity;
+}
 
-let 
-
-
-
-
-
-
-
-let query= "Polokwane";
+let query = "Polokwane";
 let key = "b4b16ao0bed60a37cdt0a5dcdf865c3b";
-let apiUrl = 'https://api.shecodes.io/weather/v1/current?query=${query}&key=${key}'
-axios.get
+let apiUrl =
+  "https://api.shecodes.io/weather/v1/current?query=Polokwane&key=b4b16ao0bed60a37cdt0a5dcdf865c3b&units=metric";
+axios.get(apiUrl).then(tempDisplay);
