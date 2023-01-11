16bd08a5e22f8ff2c1c80ac4f4d377221b79c10b
Mmaserufe Sekalo <maserufec@gmail.com>
2023-01-11 16:35:19 +0200

    added variables
    api key, url and the query

--- week7/src/app.js
+++ week7/src/app.js
@@ -1,3 +1,15 @@
 let cityElement = document.querySelector("#city");
-
 cityElement.innerHTML = response.data.name;
+
+let 
+
+
+
+
+
+
+
+let query= "Polokwane";
+let key = "b4b16ao0bed60a37cdt0a5dcdf865c3b";
+let apiUrl = 'https://api.shecodes.io/weather/v1/current?query=${query}&key=${key}'
+axios.get
