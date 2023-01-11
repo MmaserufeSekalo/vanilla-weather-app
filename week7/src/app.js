37e5c58869137d6d31c73122202ae3bf68c0eb4c
Mmaserufe Sekalo <maserufec@gmail.com>
2023-01-11 18:17:03 +0200

    weather icon
    added weather icon for latest update

--- week7/src/app.js
+++ week7/src/app.js
@@ -4,15 +4,38 @@ function tempDisplay(response) {
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind-speed");
+  let day = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let now = new Date();
+  let weekDay = day[now.getDay()];
+  let hour = now.getHours();
+  if (hour < 10) {
+    hour = "0" + hour;
+  }
+  let minutes = now.getMinutes();
+  if (minutes < 10) {
+    minutes = "0" + minutes;
+  }
+  let iconElement = document.querySelector("#icon")
   city.innerHTML = response.data.city;
   temeperaturElement.innerHTML = Math.round(response.data.temperature.current);
   descriptionElement.innerHTML = response.data.condition.description;
   humidityElement.innerHTML = response.data.temperature.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
+  document.querySelector(".day").innerHTML = weekDay;
+  document.querySelector(".time").innerHTML = hour + ":" + minutes;
+  iconElement.setAttribute("src",response.data.condition.icon_url)
 }
 
-let query = "Polokwane";
-let key = "b4b16ao0bed60a37cdt0a5dcdf865c3b";
+let area = "Polokwane";
+let apiKey = "b4b16ao0bed60a37cdt0a5dcdf865c3b";
 let apiUrl =
   "https://api.shecodes.io/weather/v1/current?query=Polokwane&key=b4b16ao0bed60a37cdt0a5dcdf865c3b&units=metric";
 axios.get(apiUrl).then(tempDisplay);
